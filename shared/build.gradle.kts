d4d229630b1f1f6db4541c0d55b094625b30d967
apffjfjd <115337453+apffjfjd@users.noreply.github.com>
2024-03-26 18:09:01 +0900

    add kotlin dsl to gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,11 +23,46 @@ kotlin {
             isStatic = true
         }
     }
-    
-    sourceSets {
-        commonMain.dependencies {
-            // put your Multiplatform dependencies here
+
+    // JVM 타겟 설정 추가
+    jvm {
+        compilations.all {
+            kotlinOptions {
+                // JVM 타겟의 Kotlin 코드를 컴파일하기 위한 JVM 버전을 지정
+                jvmTarget = "11" // 필요에 따라 "11" 등으로 변경 가능
+            }
         }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                // 공통 의존성 설정
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                // JVM 플랫폼 특정 의존성 추가
+            }
+        }
+
+        /*// iOS 플랫폼 소스 세트 설정 추가
+        val iosMain by getting {
+            dependencies {
+                // iOS 플랫폼 특정 의존성 추가
+                // 예: implementation("com.example:ios-library:1.0.0")
+            }
+        }
+        val iosX64Main by getting {
+            dependsOn(iosMain)
+        }
+        val iosArm64Main by getting {
+            dependsOn(iosMain)
+        }
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosMain)
+        }*/
     }
 }
 
